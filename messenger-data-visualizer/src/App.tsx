5c939fabcb2e77d434851125ac12c849a8518d01
Siddhartha Gorti <siddthekid2012@gmail.com>
2018-12-09 16:36:53 -0800

    Added reactstrap

--- messenger-data-visualizer/src/App.tsx
+++ messenger-data-visualizer/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import logo from './logo.svg';
 
+import { Button } from 'reactstrap';
+
 class App extends React.Component {
   public render() {
     return (
@@ -14,6 +16,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button color="danger">Danger!</Button>
       </div>
     );
   }
